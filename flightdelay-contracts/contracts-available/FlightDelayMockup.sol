9ce5ce0af1e7ce0d8dcd77f1fde4231233b2cd6b
CherryQii <91779865+CherryQii@users.noreply.github.com>
2023-02-03 19:36:40 +0800

    Update FlightDelayMockup.sol

--- flightdelay-contracts/contracts-available/FlightDelayMockup.sol
+++ flightdelay-contracts/contracts-available/FlightDelayMockup.sol
@@ -8,23 +8,23 @@ import "@etherisc/gif-interface/contracts/services/InstanceOperatorService.sol";
 import "@etherisc/gif-interface/contracts/Product.sol";
 
 contract FlightDelayMockup is Product {
-    // 合约主要功能
+    // 触发保单申请事件，与前端交互
     event LogAppliedForPolicy(
-        bytes32 _carrierFlightNumber,
-        // 字段什么意思
-        bytes32 _departureYearMonthDay,
-        uint256 _departureTime,
-        uint256 _arrivalTime,
-        uint256[] _payoutOptions
+        bytes32 _carrierFlightNumber, // 航班号
+        bytes32 _departureYearMonthDay, // 出发日期
+        uint256 _departureTime, // 出发时间
+        uint256 _arrivalTime, // 到达时间
+        uint256[] _payoutOptions // 赔偿金额
     );
 
     bytes32 public constant NAME = "FlightDelayEtheriscOracle";
     bytes32 public constant VERSION = "0.1.11";
     bytes32 public constant POLICY_FLOW = "PolicyFlowDefault";
 
+    // 初始化合约：主要是初始化Product合约
     constructor(address _productController)
     public
-    Product(_productController, NAME, POLICY_FLOW)
+    Product(_productController, NAME, POLICY_FLOW) //
     {}
 
     function applyForPolicy(
